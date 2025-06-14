6c90fd11e45d8cbed9ac687bc98cb18519879a88
Khanh Nguyen <nguyenk8@mymail.nku.edu>
2025-06-14 14:20:37 -0400

    update ui + add more function

--- public/electron.js
+++ public/electron.js
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 350,
-    height: 500,
+    width: 450,
+    height: 600,
     resizable: false,
     alwaysOnTop: true,
     skipTaskbar: true,
@@ -13,7 +13,7 @@ function createWindow() {
       contextIsolation: false,
     },
   });
-  win.loadURL("http://localhost:3001");
+  win.loadURL("http://localhost:3002");
   win.setMennu(null);
 }
 app.whenReady().then(createWindow);
